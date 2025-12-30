423c6476c300af502fc0732b3d4d1df11cc7f1c6
pkbmusahamandiri <websitepkbmusahamandiri@gmail.com>
2025-12-30 15:12:00 +0700

    mode.php

--- app/Models/video.php
+++ app/Models/video.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    protected $fillable = ['judul', 'url', 'deskripsi']; // sesuaikan kolomnya
+    use HasFactory;
+
+    protected $table = 'videos'; // pastikan nama tabel ini benar
+
+    protected $fillable = [
+        'judul',
+        'url',
+        'deskripsi',
+    ];
 }
